76eaf457c0a2977fd1d69d1de1cb4c7245abb241
SenujaUdugampola <2022cs204@stu.ucsc.cmb.ac.lk>
2025-10-18 02:30:28 +0530

    Revert "Update src/main/java/com/leo/pillpathbackend/controller/NotificationController.java"
    This reverts commit 2b0cf69a0d5965792dc1e6d5cfc2c66d4ff052d3.

--- src/main/java/com/leo/pillpathbackend/controller/NotificationController.java
+++ src/main/java/com/leo/pillpathbackend/controller/NotificationController.java
@@ -40,11 +40,8 @@ public class NotificationController {
      * Mark notification as read
      */
     @PutMapping("/{id}/read")
-    public ResponseEntity<Void> markAsRead(
-            @PathVariable Long id,
-            @RequestParam Long userId,
-            @RequestParam String userType) {
-        notificationService.markAsRead(id, userId, userType);
+    public ResponseEntity<Void> markAsRead(@PathVariable Long id) {
+        notificationService.markAsRead(id);
         return ResponseEntity.ok().build();
     }
     
